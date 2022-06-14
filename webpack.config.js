fb49d54d42360aa16ee55a1a5dca98496605aa04
Grzegorz Maniak <X00189661@myTUDublin.ie>
2022-06-14 16:02:30 +0100

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,17 +3,8 @@ const path = require('path');
 module.exports = {
     watch: true,
     entry: './src/index.ts',
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-        ],
-    },
     resolve: {
-        extensions: [ '.tsx', '.ts', '.js' ],
+        extensions: [ '.ts', '.js' ],
     },
     output: {
         filename: 'bundle.js',
